5825a0de384c9a1c7aaf95423d1659bd4339073f
Karl Hobley <karl@kaed.uk>
2020-11-17 17:44:53 +0000

    Initial commit

---
+++ wagtail_foo/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class WagtailFooAppConfig(AppConfig):
+    label = "wagtail_foo"
+    name = "wagtail_foo"
+    verbose_name = "Wagtail Foo"
---
+++ wagtail_foo/test/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class WagtailFooTestAppConfig(AppConfig):
+    label = "wagtail_foo_test"
+    name = "wagtail_foo.test"
+    verbose_name = "Wagtail Foo tests"
